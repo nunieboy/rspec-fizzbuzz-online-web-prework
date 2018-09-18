a551e3ce2ce70e6690e0ae2db0109ab1ccd7d1c0
Pablo Eusebio <nunieboy88@gmail.com>
2018-09-18 17:20:44 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-
+require 'pry'
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
+  elsif in % 5 == 0
+    "Buzz"
   end
 end
